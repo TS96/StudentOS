a1dad3697adb53d5d1962e6e4a4206cd8ed0b1c2
Tarek <tareksaidee@hotmail.com>
2017-04-30 01:13:45 -0400

    Implement swapper methods

--- StudentOS/Swapper.cpp
+++ StudentOS/Swapper.cpp
@@ -1,12 +1,83 @@
 #include "Swapper.h"
+#include "Memory.h"
 
+void siodrum(int jobnum, int jobsize, int coreaddress, int direction);
 
 
 Swapper::Swapper()
 {
+	swappingIn = false;
+	swappingOut = false;
 }
 
+bool Swapper::isSwappingIn() {
+	return swappingIn;
+}
 
-Swapper::~Swapper()
-{
+bool Swapper::isSwappingOut() {
+	return swappingOut;
+}
+
+void Swapper::setSwappingIn(bool status) {
+	swappingIn = status;
+}
+
+void Swapper::setSwappingOut(bool status) {
+	swappingOut = status;
+}
+
+PCB* Swapper::getJobBeingSwapped() {
+	return beingSwapped;
+}
+
+void Swapper::addToLTS(PCB* newJob) {
+	LTS.push_back(newJob);
+	std::sort(LTS.begin(), LTS.end(), Memory::sortByRemainingTime);
+}
+
+void Swapper::runFromLTS(int &a, int p[], Memory& memory) {
+	cout << "Run from LTS " << LTS.size() << endl;
+	swapFromLTS(a, p, memory);
+	a = 1;
+}
+
+bool Swapper::swapIn(int &a, int p[], PCB *temp, int memoryPos) {
+	if (!swappingIn && !swappingOut) {
+		siodrum(temp->getJobNumber(), temp->getJobSize(), memoryPos, 0);
+		swappingIn = true;
+		beingSwapped = temp;
+		return true;
+	}
+	return false;
+}
+
+void Swapper::swapOut(int &a, int p[], Memory& memory) {
+	if (!swappingIn && !swappingOut && !swapOutQ.empty()) {
+		if ((swapOutQ.front()->isDoingIO() || swapOutQ.front()->getPendingIO() == 0 || !swapOutQ.front()->isInMemory()) && !swapOutQ.front()->isTooBig()) {
+			swapOutQ.pop();
+			return;
+		}
+		swapOutQ.front()->setBlocked(true);
+		siodrum(swapOutQ.front()->getJobNumber(), swapOutQ.front()->getJobSize(), swapOutQ.front()->getMemoryPos(), 1);
+		swappingOut = true;
+		memory.deleteFromMemory(swapOutQ.front());
+		if (swapOutQ.front()->isTooBig()) {
+			swapOutQ.front()->setTooBig(false);
+			swapOutQ.front()->setBlocked(false);
+			LTS.push_back(swapOutQ.front());
+			std::sort(LTS.begin(), LTS.end(), memory.sortByRemainingTime);
+		}
+		swapOutQ.pop();
+	}
+}
+
+void Swapper::swapFromLTS(int &a, int p[], Memory& memory) {
+	if (!LTS.empty()) {
+		PCB* temp = LTS.back();
+		int memoryPos = memory.findMemPos(temp);
+		if (memoryPos != -1) {
+			if (swapIn(a, p, temp, memoryPos))
+				LTS.pop_back();
+		}
+	}
 }
--- StudentOS/Swapper.h
+++ StudentOS/Swapper.h
@@ -3,6 +3,7 @@
 #include <queue>
 #include "PCB.h"
 #include "Memory.h"
+#include <iostream>
 using namespace std;
 
 class Swapper
@@ -11,11 +12,11 @@ private:
 	PCB* beingSwapped;
 	vector<PCB*> LTS;
 	queue<PCB*> swapOutQ;
-	bool swappingIn = false;
-	bool swappingOut = false;
+	bool swappingIn;
+	bool swappingOut;
 public:
 	Swapper();
-	void runFromLTS(int &, int[]);
+	void runFromLTS(int &, int[], Memory&);
 	bool swapIn(int &, int[], PCB *, int);
 	void swapOut(int &, int[], Memory&);
 	void swapFromLTS(int &, int[], Memory&);
